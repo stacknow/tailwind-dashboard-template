8bb8599ae67f3b1ca948080df3aff04649725fee
aenosys <aenosys@gmail.com>
2024-01-04 13:06:15 -0500

    Update FourWorlds.js

--- functions/FourWorlds.js
+++ functions/FourWorlds.js
@@ -1,4 +1,24 @@
-// helloWorld.js
-export default async (req, res) => {
-    res.send("Hello, World!");
+import fetch from 'node-fetch';
+
+const logicFunction = async (req, res) => {
+    try {
+        const apiUrl = 'https://api.flatfilemaker.com/sqlman/table/vercel_db/websites'; // API endpoint
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`API request failed with status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        // You can now work with the data from the API response as needed
+        // Perform any additional logic with 'data'
+
+        res.json(data); // Send the API response as the HTTP response
+    } catch (error) {
+        console.error('Error in logicFunction:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
+
+export default logicFunction;
