1bac1dff29eb7acfad6b4c816b4de130bfb782a8
aenosys <aenosys@gmail.com>
2024-01-04 12:25:49 -0500

    Update helloWorld.js

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,4 +1,16 @@
-// helloWorld.js
-export default async (req, res) => {
-    res.send("Hello, World!");
-};
+// myRouteHandler.js
+
+// Import necessary modules
+const express = require('express');
+
+// Create an instance of the Express Router
+const router = express.Router();
+
+// Define a route handler function
+router.get('/', (req, res) => {
+    // Respond with a JSON message
+    res.json({ message: 'Hello, World!' });
+});
+
+// Export the router
+module.exports = router;
