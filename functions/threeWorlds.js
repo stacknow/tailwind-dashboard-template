d18caa41e037f45733be56082400205c5006921a
aenosys <aenosys@gmail.com>
2024-01-04 12:51:18 -0500

    Update threeWorlds.js

--- functions/threeWorlds.js
+++ functions/threeWorlds.js
@@ -1,41 +1,31 @@
-// logicFunction.js
 import fetch from 'node-fetch';
 
 const fetchData = async () => {
-    try {
-        const response = await fetch('https://api.flatfilemaker.com/sqlman/table/vercel_db/websites');
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
+  try {
+    const apiUrl = 'https://api.flatfilemaker.com/sqlman/table/vercel_db/websites';
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
     }
+
+    const data = await response.json();
+    return data.map((website) => website.website_name);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 const displayWebsiteNames = async () => {
-    const data = await fetchData();
-    if (!data) {
-        console.error('No data available');
-        return;
-    }
+  const websiteNames = await fetchData();
 
-    const websiteNames = data.map((website) => website.website_name);
+  if (websiteNames.length === 0) {
+    console.error('No website names available');
+    return;
+  }
 
-    // Display website names in the console or on the webpage
-    console.log(websiteNames);
-
-    // If you're working with a web page, you can also display them in the HTML document
-    // For example, assuming you have an HTML element with the id "websiteList"
-    const websiteListElement = document.getElementById('websiteList');
-    if (websiteListElement) {
-        websiteNames.forEach((name) => {
-            const listItem = document.createElement('li');
-            listItem.textContent = name;
-            websiteListElement.appendChild(listItem);
-        });
-    }
+  console.log(websiteNames);
 };
 
-export { fetchData, displayWebsiteNames };
+displayWebsiteNames();
