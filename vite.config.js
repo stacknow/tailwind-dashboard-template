2fb329e030a24c8dec4c43d00596ccfe40b4664e
stacknow <stacknow@outlook.com>
2025-03-28 21:01:32 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // --- ADD THIS LINE ---
+  base: '/', 
+  // --------------------
   define: {
     'process.env': process.env
   },
